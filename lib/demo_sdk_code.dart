853608662b61d799ef4d30c93675646013a11089
manankoyawalajainam <manankoyawala.jainam@gmail.com>
2025-05-27 14:16:15 +0530

    get car and get fruits added

--- lib/demo_sdk_code.dart
+++ lib/demo_sdk_code.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-List<String> carItems = [
+List<String> carBrands = [
   "Tesla",
   "Honda",
   "Mahindra",
@@ -9,16 +9,38 @@ List<String> carItems = [
   "Hyundai"
 ];
 
-class LoadItems extends StatelessWidget {
-  const LoadItems({super.key});
+List<String> fruits = ["Apple", "Banana", "Orange", "Peach"];
+
+class GetCars extends StatelessWidget {
+  const GetCars({super.key});
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: carItems.length,
+      itemCount: carBrands.length,
       itemBuilder: (context, index) {
         return Text(
-          carItems[index],
+          carBrands[index],
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.w500,
+          ),
+        );
+      },
+    );
+  }
+}
+
+class GetFruits extends StatelessWidget {
+  const GetFruits({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: fruits.length,
+      itemBuilder: (context, index) {
+        return Text(
+          fruits[index],
           style: TextStyle(
             fontSize: 18,
             fontWeight: FontWeight.w500,
